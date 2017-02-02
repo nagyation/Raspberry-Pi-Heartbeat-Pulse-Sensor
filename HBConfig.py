d874705f8f9692f55a10b7c32fbd747e7cfe52a8
Mahmoud Nagy <mnagy1312@gmail.com>
2017-02-03 00:03:47 +0200

    Customized some things to my needs

---
+++ HBConfig.py
@@ -0,0 +1,9 @@
+"""
+Configuration for the HB sensor
+it's anoher way for changing the parameters than the constructor
+"""
+class HBConfig:
+    HBConfig.CHANNEL = 0 # The Channel where the HB sensor connected to.
+    # The bus and the device
+    HBConfig.BUS     = 0 
+    HBConfig.DEVICE  = 0
